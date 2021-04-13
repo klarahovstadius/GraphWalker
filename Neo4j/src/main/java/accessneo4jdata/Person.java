30c156bbd8f8fca0f6335e5a5d89a4f07d179397
khovstadius <klara.hovstadius@sixsentix.com>
2021-04-13 16:22:12 +0200

    Neo4j example connected to my database

---
+++ Neo4j/src/main/java/accessneo4jdata/Person.java
@@ -0,0 +1,34 @@
+package accessneo4jdata;
+
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+/**
+ * The Person object has a first name and a last name.
+ * There is also an ID object that is configured to be automatically generated so that you need not do so.
+ */
+@Node
+public class Person {
+
+	@Id @GeneratedValue private Long id;
+
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
---
+++ Neo4j/src/main/java/accessneo4jdata/PersonRepository.java
@@ -0,0 +1,22 @@
+package accessneo4jdata;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+/**
+ * This repository is an interface and lets you perform various operations that involve Person objects.
+ * It gets these operations by extending the PagingAndSortingRepositry interface defined in Spring Data Commons.
+ * At runtime, Spring Data REST automatically creates an implementation of this interface.
+ * Then it uses the @RepositoryRestResource annotation to direct Spring MVC to create RESTful endpoints at /people.
+ * @RepositoryRestResource is not required for a repository to be exported. It is used only to change the export details,
+ * such as using /people instead of the default value of /persons.
+ */
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+	List<Person> findByLastName(@Param("name") String name);
+
+}
